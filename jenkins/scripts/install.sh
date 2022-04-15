951222e70ad5cf0719b127c9e1f3ea86b529d535
mrtnbm <behmmartin14@googlemail.com>
2022-04-15 18:06:33 +0200

    c

--- jenkins/scripts/install.sh
+++ jenkins/scripts/install.sh
@@ -1,6 +1,6 @@
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt install nodejs
-npm install snyk@latest -g
-export PATH=$PATH:${env.WORKSPACE}
-curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b ${WORKSPACE}
-curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b ${WORKSPACE}
+sudo npm install snyk@latest -g
+sudo export PATH=$PATH:${env.WORKSPACE}
+sudo curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b ${WORKSPACE}
+sudo curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b ${WORKSPACE}
