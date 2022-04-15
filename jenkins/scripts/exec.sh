95a44044a157a8954dcac79d90817d98b13b9d6d
mrtnbm <behmmartin14@googlemail.com>
2022-04-15 18:51:28 +0200

    c

--- jenkins/scripts/exec.sh
+++ jenkins/scripts/exec.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #snyk test
-${HOME_WORKSPACE}/syft packages dir:. --file ./sbom.json -o syft-json -s AllLayers -vv
-${HOME_WORKSPACE}/grype sbom:./sbom.json --add-cpes-if-none true --file ./vuln.json --fail-on high -o json -s AllLayers -vv
+${HOME_WORKSPACE}/syft packages dir:. --file ./sbom.json -o syft-json -s AllLayers -v
+${HOME_WORKSPACE}/grype sbom:./sbom.json --add-cpes-if-none true --file ./vuln.json --fail-on high -o json -s AllLayers -v
