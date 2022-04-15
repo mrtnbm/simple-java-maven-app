1624f26c58d24bf6042a371d1ed2bca9480489ed
mrtnbm <behmmartin14@googlemail.com>
2022-04-15 20:38:23 +0200

    c

--- jenkins/scripts/exec.sh
+++ jenkins/scripts/exec.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 snyk auth ${SNYK_TOKEN}
 snyk test ${HOME_WORKSPACE} --all-projects --detection-depth 100 --print-deps --show-vulnerable-paths=all --prune-repeated-subdependencies --json
-${HOME_WORKSPACE}/syft packages dir:. --file ./sbom.json -o syft-json -s AllLayers -v
+##${HOME_WORKSPACE}/syft packages dir:. --file ./sbom.json -o syft-json -s AllLayers -v
 #${HOME_WORKSPACE}/grype sbom:./sbom.json --add-cpes-if-none --file ./vuln.json -o json -s AllLayers -v
 #--fail-on high
--- jenkins/scripts/install.sh
+++ jenkins/scripts/install.sh
@@ -3,5 +3,5 @@ curl -sL https://deb.nodesource.com/setup_16.x | bash -
 apt-get install -y nodejs
 #apt install -y build-essential
 npm install snyk@latest -g
-curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b ${HOME_WORKSPACE}
-curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b ${HOME_WORKSPACE}
+##curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b ${HOME_WORKSPACE}
+##curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b ${HOME_WORKSPACE}
