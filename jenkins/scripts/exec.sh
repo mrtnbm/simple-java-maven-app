41201d5b2d2a30d72380d22291110b8a294bb58a
mrtnbm <behmmartin14@googlemail.com>
2022-04-15 20:51:02 +0200

    c

--- jenkins/scripts/exec.sh
+++ jenkins/scripts/exec.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 snyk auth ${SNYK_TOKEN}
-snyk test /root/.m2 --all-projects --print-deps --show-vulnerable-paths=all --prune-repeated-subdependencies --json
+snyk test ${HOME_WORKSPACE}/ --all-projects --print-deps --show-vulnerable-paths=all --prune-repeated-subdependencies --json
 ##${HOME_WORKSPACE}/syft packages dir:. --file ./sbom.json -o syft-json -s AllLayers -v
 #${HOME_WORKSPACE}/grype sbom:./sbom.json --add-cpes-if-none --file ./vuln.json -o json -s AllLayers -v
 #--fail-on high
