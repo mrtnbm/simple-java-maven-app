b3594c7d69227a18309a6dc04db0e0175be868b7
mrtnbm <behmmartin14@googlemail.com>
2022-04-15 18:31:52 +0200

    c

--- jenkins/scripts/install.sh
+++ jenkins/scripts/install.sh
@@ -1,4 +1,4 @@
-sudo apt install npm
+apt install npm
 npm install snyk@latest -g
 curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b ${HOME_WORKSPACE}
 curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b ${HOME_WORKSPACE}
