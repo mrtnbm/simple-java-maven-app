bb701b5afa29fff8630bae9f488e1fe535e886d1
mrtnbm <behmmartin14@googlemail.com>
2022-04-15 19:45:42 +0200

    c

--- jenkins/scripts/exec.sh
+++ jenkins/scripts/exec.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+snyk auth
 snyk test
 ${HOME_WORKSPACE}/syft packages dir:. --file ./sbom.json -o syft-json -s AllLayers -v
 #${HOME_WORKSPACE}/grype sbom:./sbom.json --add-cpes-if-none --file ./vuln.json -o json -s AllLayers -v
