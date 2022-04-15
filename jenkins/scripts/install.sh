565cf60bf851da69ec6ca6cc0077288272150e16
mrtnbm <behmmartin14@googlemail.com>
2022-04-15 18:31:15 +0200

    c

--- jenkins/scripts/install.sh
+++ jenkins/scripts/install.sh
@@ -1,5 +1,4 @@
-curl -sL https://deb.nodesource.com/setup_16.x | -E bash -
-apt install nodejs
+sudo apt install npm
 npm install snyk@latest -g
 curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b ${HOME_WORKSPACE}
 curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b ${HOME_WORKSPACE}
